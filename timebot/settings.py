17c1fefa55221ae86849fc7ca8782e6a2cde7ba4
stephen cyron <stephen.cyron@fdmgroup.com>
2022-03-03 14:18:47 -0500

    adjusted settings.py

--- timebot/settings.py
+++ timebot/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -129,6 +126,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
