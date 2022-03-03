95c1a1f7bcc4cbe7faf1b1f01692ffa4688cc296
stephen cyron <stephen.cyron@fdmgroup.com>
2022-03-03 15:37:23 -0500

    Got rid of load_dotenv

--- timebot/settings.py
+++ timebot/settings.py
@@ -11,14 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from ast import Import
 import os
-import load_dotenv
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
